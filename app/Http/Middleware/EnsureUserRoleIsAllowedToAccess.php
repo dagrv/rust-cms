33edfc36f4a2a1e99a168e202b8e52908cbb1258
Rust <rust.91@icloud.com>
2021-05-03 18:23:20 +0200

    User Access Roles Laravel Middleware

---
+++ app/Http/Middleware/EnsureUserRoleIsAllowedToAccess.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class EnsureUserRoleIsAllowedToAccess
+{
+    /**
+     * Handle an incoming request.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $userRole = auth()->user()->role;
+            $currentRoleName = Route::currentRouteName();
+
+            if (in_array($currentRoleName, $this->userAccessRole()[$userRole])) {
+                return $next($request);
+            } else {
+                abort(403, 'Unauthorized');
+            }
+        } catch (\Throwable $th) {
+            abort(403, 'Not Allowed');
+        }
+    }
+
+    /**
+     * All of accessible resource for a given user
+     *
+     *
+     * @return void
+     */
+    private function userAccessRole()
+    {
+        return [
+            'user' => [
+                'dashboard'
+            ],
+
+            'admin' => [
+                'pages',
+                'navigation-menus'
+            ]
+        ];
+    }
+}
